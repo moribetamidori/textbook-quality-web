f2bb0292c1a8efd1b8cfc7bb02c0975d73b44419
Lucy Qiu <lucy1049684100@gmail.com>
2023-11-25 23:19:24 -0500

    s

---
+++ textbook_quality_web/pages/api/generateTopics.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const response = await fetch("http://127.0.0.1:8000/generate_topics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      // Check if the status is 'success' and if the data contains 'file'
+      if (data.status === "success" && data.file) {
+        // Fetch the file content
+        const fileResponse = await fetch(data.file);
+        if (fileResponse.ok) {
+          const fileData = await fileResponse.json();
+          // Add the file content to the response data
+          data.fileContent = fileData;
+        }
+      }
+      res.status(200).json(data);
+    } else {
+      res.status(response.status).json({ message: "Generation failed." });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed." });
+  }
+}
---
+++ textbook_quality_web/pages/index.tsx
@@ -0,0 +1,87 @@
+import { useState, FormEvent } from "react";
+
+interface IGenerateTopicsResponse {
+  status: string;
+  data: any;
+}
+
+export default function Home() {
+  const [subject, setSubject] = useState<string>("");
+  const [outFile, setOutFile] = useState<string>("");
+  const [iterations, setIterations] = useState<number>(0);
+  const [status, setStatus] = useState<string>("");
+  const [responseData, setResponseData] = useState<any>(null); // Initialize the state
+
+  console.log("responseData", responseData)
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+  
+    const response = await fetch("/api/generateTopics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        subject,
+        out_file: outFile,
+        iterations,
+      }),
+    });
+  
+    const data: IGenerateTopicsResponse = await response.json();
+    setStatus(data.status);
+    setResponseData(data.data); // Set the state with the returned data
+  
+    // If the generation was successful, call the augmentTopics function
+    if (data.status === "success") {
+      handleAugmentTopics(outFile);
+    }
+  };
+
+  const handleAugmentTopics = async (inFile: string) => {
+    const response = await fetch("/api/augmentTopics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        in_file: inFile,
+        out_file: "augmented_" + inFile, // You can modify this as needed
+        domain: "python", // You can modify this as needed
+      }),
+    });
+  
+    const data: IGenerateTopicsResponse = await response.json();
+    setStatus(data.status);
+    setResponseData(data.data); // Set the state with the returned data
+  };
+  return (
+    <main className="min-h-screen">
+      {/* ...existing code... */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+          placeholder="Subject"
+        />
+        <input
+          type="text"
+          value={outFile}
+          onChange={(e) => setOutFile(e.target.value)}
+          placeholder="Output File"
+        />
+        <input
+          type="number"
+          value={iterations}
+          onChange={(e) => setIterations(Number(e.target.value))}
+          placeholder="Iterations"
+        />
+        <button type="submit">Generate Topics</button>
+      </form>
+      {status && <p>Status: {status}</p>}
+      {responseData && <p>Response: {JSON.stringify(responseData)}</p>}
+    </main>
+  );
+}
